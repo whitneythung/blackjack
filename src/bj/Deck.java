1e4ee7e36f5b19257e219ff2c95bc6a49374600b
whitnayayay <whitneythung@yahoo.com>
2020-09-21 17:07:28 -0700

    updated some files

--- src/bj/Deck.java
+++ src/bj/Deck.java
@@ -1,5 +1,27 @@
+//This class represents a deck of cards
+
+
 package bj;
+import java.util.*;
 
 public class Deck extends Card {
+	private Stack<String> cards;
+	private int cardsLeft;
 
+	//want to construct a deck with 52 cards
+	public Deck {
+		this.cards = new Stack<String>(); 
+		this.cardsLeft = 52;
+	}
+	
+	public boolean isEmpty() {
+		if (size = 0) {
+			return true;
+		}
+		return false;
+	}
+	
+	public int size() {
+		return this.cardsLeft;
+	}
 }
